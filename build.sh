29d429369536ef7963f4d25ac942a434d29b485b
marmooo <marmooo@users.noreply.github.com>
2026-09-19 15:11:03 +0900

    bump svgo from 4.0.2 to 4.1.0

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir -p docs
 cp -r src/* docs
-npx svgo@4.0.2 src/map.svg -o docs/map.svg
+npx svgo@4.1.0 src/map.svg -o docs/map.svg
 drop-inline-css -r src -o docs
 deno run -A bundle.js ./src/index.js > docs/index.js
 minify -r docs -o .
